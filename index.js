ae5a96a6bd7615837c3e8f37181479654df6a9a1
Brian Yarsawich <Brian.Yarsawich@townofcary.org>
2017-03-09 17:24:01 -0500

    fixed help message in getUserAddress function

--- index.js
+++ index.js
@@ -569,7 +569,7 @@ function getUserAddress(userToken, state, intent, self){
   }).finally(function(){
     if(self.attributes["address"] == undefined || self.attributes["address"] == null){
       self.handler.state = state;
-      var prompt = 'Please tell me your address so I can look up nearby public art';
+      var prompt = 'Please tell me your address so I can look up your requested information';
       self.emit(':ask', prompt, prompt);
     }
   });
